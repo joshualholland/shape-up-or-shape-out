aa383696e29cef6c7c2c9587e86c036b2ff34473
joshualholland <joshualholland@gmail.com>
2018-10-31 13:56:18 -0500

    styling issue

---
+++ scripts.js
@@ -0,0 +1,85 @@
+class Shape {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.radius = this.height /2;
+        this.name = name;
+        this.div = document.createElement('div');
+        this.div.style.width = this.width + 'px';
+        this.div.style.height = this.height + 'px';
+        document.getElementById('shapeCont').appendChild(this.div);
+    }
+
+    describe() {
+        document.getElementById('name').textContent = this.name;
+        document.getElementById('width').textContent = this.width;
+        document.getElementById('height').textContent = this.height;
+        document.getElementById('radius').textContent = this.radius;
+        document.getElementById('area').textContent = this.area();
+        document.getElementById('perimeter').textContent = this.perimeter();
+        
+    }
+    area() {
+        return this.height * this.width
+    }
+    perimeter() {
+        return (this.height + this.width) *2;
+    }
+};
+
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super(width, height)
+        this.name = 'Rectangle';
+        this.div.id = 'RectangleShape';
+        this.div.style.backgroundColor = 'green';
+    }
+
+    describe() {
+        super.describe();
+    }
+};
+
+class Square extends Shape {
+    constructor(height, width) {
+        super(height, width)
+        this.name = 'Square';
+        this.div.id = 'SquareShape';
+        this.div.style.backgroundColor = 'red';
+    }
+
+    describe() {
+        super.describe();
+    }
+};
+
+class Circle extends Shape {
+    constructor(radius) {
+        super(radius*2, radius*2);
+        this.name = 'Circle';
+        this.div.id = 'CircleShape';
+        this.div.style.backgroundColor = 'purple';
+        this.radius = radius;
+    }
+
+    describe() {
+        super.describe();
+    }
+};
+
+document.getElementById('addCircle').addEventListener('click', () => {
+    let rad = document.getElementById('circleR').value;
+    new Circle(rad)
+});
+
+document.getElementById('addSquare').addEventListener('click', () => {
+    let hi = document.getElementById('SquareSL').value;
+    let wid = document.getElementById('SquareSL').value;
+    new Square(wid, hi);
+});
+
+document.getElementById('addRect').addEventListener('click', () => {
+    let wid = document.getElementById('rectW').value;
+    let hi = document.getElementById('rectH').value;
+    new Rectangle(wid, hi);
+});
